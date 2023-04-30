6ae71c2160e7637e626b410d5213f62323da4096
iwangy <35878784+iwangy@users.noreply.github.com>
2023-04-29 23:50:13 -0700

    add completed RMI assignment

--- Java/Client.java
+++ Java/Client.java
@@ -1,22 +1,114 @@
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Client {
+    // Connect to server
+    // create an instance of the remote method
+    // RemoteMethod add = new RemoteMethod("add", newObjects[]{lhs, rhs});
+    // ObjectOutputStream to serialize the add instance
+    // Send that serialized data to the output stream
+    // OutputStream os = socket.getOutputStream();
 
     /**
      * This method name and parameters must remain as-is
      */
     public static int add(int lhs, int rhs) {
-        return -1;
+        int sum = 0;
+        try {
+            Socket socket = new Socket("localhost", PORT);
+            RemoteMethod add = new RemoteMethod("add", new Object[]{lhs, rhs});
+
+            OutputStream os = socket.getOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+     
+            oos.writeObject(add);
+            oos.flush();
+
+            InputStream is = socket.getInputStream();
+            ObjectInputStream ois = new ObjectInputStream(is);
+            RemoteMethod addRM = (RemoteMethod) ois.readObject();
+            Object[] arr = addRM.getArguments();
+            sum = (int) arr[0];
+
+            socket.close();
+            oos.close();
+            ois.close();
+            os.close();
+            is.close();
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+        return sum;
     }
     /**
      * This method name and parameters must remain as-is
      */
     public static int divide(int num, int denom) {
-        return -1;
+        int quotient = 0;
+        
+        if (denom==0) {
+            throw new ArithmeticException();
+        }
+
+        try {
+            Socket socket = new Socket("localhost", PORT);
+            RemoteMethod divide = new RemoteMethod("divide", new Object[]{num, denom});
+
+            OutputStream os = socket.getOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+     
+            oos.writeObject(divide);
+            oos.flush();
+
+            InputStream is = socket.getInputStream();
+            ObjectInputStream ois = new ObjectInputStream(is);
+            RemoteMethod divideRM = (RemoteMethod) ois.readObject();
+            Object[] arr = divideRM.getArguments();
+            quotient = (int) arr[0];
+
+            socket.close();
+            oos.close();
+            ois.close();
+            os.close();
+            is.close();
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+        return quotient;
     }
     /**
      * This method name and parameters must remain as-is
      */
     public static String echo(String message) {
-        return "";
+        String output = "";
+        try {
+            Socket socket = new Socket("localhost", PORT);
+            RemoteMethod echo = new RemoteMethod("echo", new Object[]{message});
+
+            OutputStream os = socket.getOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+     
+            oos.writeObject(echo);
+            oos.flush();
+
+            InputStream is = socket.getInputStream();
+            ObjectInputStream ois = new ObjectInputStream(is);
+            RemoteMethod echoRM = (RemoteMethod) ois.readObject();
+            Object[] arr = echoRM.getArguments();
+            output = (String) arr[0];
+            
+            socket.close();
+            oos.close();
+            ois.close();
+            os.close();
+            is.close();
+        } catch (Exception err) {
+            err.printStackTrace();
+        }
+        return output;
     }
 
     // Do not modify any code below this line
--- Java/Server.java
+++ Java/Server.java
@@ -1,5 +1,53 @@
-public class Server {
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
 
+public class Server {
+    public static void main(String[] args) {
+        try(ServerSocket server = new ServerSocket(10314);) {
+            // Read the bytes
+            // Deserialize using ObjectInputStream
+            // process the data that is sent here in the server to the methods in the server.
+            // can create another response method if there are any exceptions in the REmote Method.
+            
+            Socket client = null;
+            while ((client = server.accept()) != null) {
+                InputStream is = client.getInputStream();
+                ObjectInputStream ois = new ObjectInputStream(is);
+                RemoteMethod rm = (RemoteMethod) ois.readObject();
+
+                String method = rm.getMethodName();
+                Object arr[] = rm.getArguments();
+                Object ret = null;
+
+                if (method.equals("add")) {
+                    ret = add((int) arr[0], (int) arr[1]);
+                } else if (method.equals("divide")) {
+                    ret = divide((int) arr[0], (int) arr[1]);
+                } else {
+                    ret = echo((String) arr[0]);
+                }
+
+                RemoteMethod serial = new RemoteMethod(method, new Object[]{ret});
+                OutputStream os = client.getOutputStream();
+                ObjectOutputStream oos = new ObjectOutputStream(os);
+                oos.writeObject(serial);
+
+                oos.flush();
+                ois.close();
+                oos.close();
+                is.close();
+                os.close();
+                client.close();
+            }
+            server.close();
+        } catch (Exception err) {
+            err.printStackTrace();
+        } 
+    }
     // Do not modify any code below tihs line
     // --------------------------------------
     public static String echo(String message) { 
